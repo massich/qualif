8c3d71b4c66a7bded5e00d697523aefa6c1b5dd8
Joan Massich <sik@visor.udg.edu>
2016-12-17 18:26:20 +0100

    Added some stuff

--- auto/description.el
+++ auto/description.el
@@ -4,7 +4,7 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "a4paper" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("color" "usenames") ("geometry" "paper=a4paper" "textwidth=160mm" "hmargin=2.5cm" "vmargin=1.5cm") ("xcolor" "usenames" "dvipsnames") ("biblatex" "style=numeric-comp" "backend=biber" "maxcitenames=10" "mincitenames=10" "maxbibnames=99" "minbibnames=99") ("inputenc" "latin1") ("babel" "francais") ("aeguill" "cyr")))
+                     '(("color" "usenames") ("geometry" "paper=a4paper" "textwidth=160mm" "hmargin=2.5cm" "vmargin=1.5cm") ("xcolor" "usenames" "dvipsnames") ("biblatex" "style=numeric" "backend=biber" "maxcitenames=10" "mincitenames=10" "maxbibnames=99" "minbibnames=99" "sorting=none") ("inputenc" "latin1") ("babel" "francais") ("aeguill" "cyr")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
@@ -12,6 +12,7 @@
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "article"
